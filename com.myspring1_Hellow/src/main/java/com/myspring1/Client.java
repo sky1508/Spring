fcd87f257ef95383851a8ed13d28916a66446285
sky1508 <sky12345085@yahoo.com>
2017-07-11 22:02:08 +0530

    Added ApplicationContext instead of Resource & BeanFactory

--- com.myspring1_Hellow/src/main/java/com/myspring1/Client.java
+++ com.myspring1_Hellow/src/main/java/com/myspring1/Client.java
@@ -1,20 +1,15 @@
 package com.myspring1;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
 
 	public static void main(String[] args) {
-		Resource res = new ClassPathResource("spring/spring.xml");
-		BeanFactory factory = new XmlBeanFactory(res);
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring.xml");
  
-		Object o = factory.getBean("id1");
-		HelloWorld wb = (HelloWorld)o;
- 
-		wb.show();
+		HelloWorld obj = (HelloWorld) context.getBean("hello");
+		obj.show();
 
 	}
 }
