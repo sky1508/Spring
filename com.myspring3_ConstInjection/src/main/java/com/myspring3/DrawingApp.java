0ec1ddb0da4a10daffee13ac83e2c0c7480b5e5c
sky1508 <sky12345085@yahoo.com>
2017-07-12 22:57:29 +0530

    Added Spring Constructor Injection

---
+++ com.myspring3_ConstInjection/src/main/java/com/myspring3/DrawingApp.java
@@ -0,0 +1,15 @@
+package com.myspring3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring.xml");
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring/spring.xml"));
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+	}
+
+}
