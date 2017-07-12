b97085ae6dcc60a84e072cb39c6a26772e2dc2cd
sky1508 <sky12345085@yahoo.com>
2017-07-12 23:02:53 +0530

    Added Spring Constructor Injection with 2nd parameter

--- com.myspring3_ConstInjection/src/main/java/com/myspring3/Triangle.java
+++ com.myspring3_ConstInjection/src/main/java/com/myspring3/Triangle.java
@@ -3,21 +3,32 @@ package com.myspring3;
 public class Triangle {
 	
 	private String type;
+	private int height;
 	
+	//Values are injected into this constructor
 	public Triangle(String type){
 		this.type = type;
+	}
+	
+	public Triangle(String type, int height){
+		this.type = type;
+		this.height = height;
 	}
 	
 	public String getType() {
 		return type;
 	}
-
+	
 	/*public void setType(String type) {
 		this.type = type;
 	}*/
 
+	public int getHeight() {
+		return height;
+	}
+
 	public void draw(){
-		System.out.println(getType()+" Triangle Drawn");
+		System.out.println(getType()+" Triangle Drawn "+getHeight());
 	}
 
 }
