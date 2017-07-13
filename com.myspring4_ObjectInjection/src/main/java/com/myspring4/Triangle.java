9d3ff1429648aaf47316a90ff0ce6920a316f406
sky1508 <sky12345085@yahoo.com>
2017-07-13 22:11:08 +0530

    Added Spring Object Injection & Inner Bean

---
+++ com.myspring4_ObjectInjection/src/main/java/com/myspring4/Triangle.java
@@ -0,0 +1,46 @@
+package com.myspring4;
+
+public class Triangle {
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
+	
+	public Point getPointA() {
+		return pointA;
+	}
+
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+
+	public void draw(){
+		System.out.println("PointA are "+getPointA().getX()+" "+getPointA().getY());
+		System.out.println("PointB are "+getPointB().getX()+" "+getPointB().getY());
+		System.out.println("PointC are "+getPointC().getX()+" "+getPointC().getY());
+		
+	}
+
+}
